8e263294173238ec8c173e3ba03741160bf35740
Moritz Wagner <moritz.wagner06@gmx.at>
2024-08-06 11:00:56 +0200

    #31 enemy becomes aware of the player

---
+++ Assets/Scripts/aiChase.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aiChase : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize(); 
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        print(angle);
+
+        if (angle > 90 || angle < -90)
+        {
+            transform.localScale = new Vector3(6, -6, 6);
+        } else
+        {
+            transform.localScale = new Vector3(6, 6, 6);
+        }
+
+        if (distance < 10)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+    }
+}
