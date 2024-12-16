61badd3e41e13d6c665d5e4aa3ebd1de0f56b4c1
GitChrisHTL <c.ursprung@HTBLALeonding.onmicrosoft.com>
2024-12-16 22:15:26 +0100

    almost custom movement

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Button buttonLeft;
+    public Button buttonRight;
+    private bool waitingForInput = false;
+    public PlayerMovement playerMovement;
+
+    private KeyCode keyForLeft = KeyCode.None;
+    private KeyCode keyForRight = KeyCode.None;
+
     public void PlayGame()
     {
         // right now its the index File->Build Settings
@@ -12,6 +21,100 @@ public class NewBehaviourScript : MonoBehaviour
         // sets the next level "SceneManager.GetActiveScene().buildIndex + 1"
         SceneManager.LoadScene(1);
     }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+
+    // Controls "Level"
+    public void Menu() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ChangeMoveButton(GameObject targetImage)
+    {
+        waitingForInput = true;
+        Debug.Log("Bitte eine Taste für " + targetImage.name + " drücken!");
+    }
+
+    void Update()
+    {
+        if (waitingForInput)
+        {
+            foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    if (key != KeyCode.None)
+                    {
+                        UpdateKeyImage(key);
+
+                        waitingForInput = false;
+                        Debug.Log("Taste gedrückt: " + key.ToString());
+                    }
+                }
+            }
+        }
+    }
+
+    void UpdateKeyImage(KeyCode key)
+    {
+        string path = "Menu/Controls/KeyImages/";
+
+        Sprite newSprite = null;
+
+        if (key == KeyCode.A)
+        {
+            newSprite = Resources.Load<Sprite>(path + "A");
+        }
+        else if (key == KeyCode.D)
+        {
+            newSprite = Resources.Load<Sprite>(path + "D");
+        }
+        else if (key == KeyCode.LeftArrow)
+        {
+            newSprite = Resources.Load<Sprite>(path + "LeftArrow");
+        }
+        else if (key == KeyCode.RightArrow)
+        {
+            newSprite = Resources.Load<Sprite>(path + "RightArrow");
+        }
+
+        if (newSprite != null)
+        {
+            if (buttonLeft != null && waitingForInput && keyForLeft == KeyCode.None)
+            {
+                buttonLeft.GetComponent<Image>().sprite = newSprite;
+                keyForLeft = key; // Speichern der Taste für Move Left
+                playerMovement.SetControls(keyForLeft, keyForRight, playerMovement.jumpKey, playerMovement.dashKey, playerMovement.interactKey);
+            }
+            else if (buttonRight != null && waitingForInput && keyForRight == KeyCode.None)
+            {
+                buttonRight.GetComponent<Image>().sprite = newSprite;
+                keyForRight = key; // Speichern der Taste für Move Right
+                playerMovement.SetControls(keyForLeft, keyForRight, playerMovement.jumpKey, playerMovement.dashKey, playerMovement.interactKey);
+            }
+        }
+        else
+        {
+            Debug.LogError("Sprite konnte nicht geladen werden! Überprüfe den Pfad.");
+        }
+    }
+
+    public void SaveChanges()
+    {
+        PlayerPrefs.SetString("MoveLeftKey", keyForLeft.ToString());
+        PlayerPrefs.SetString("MoveRightKey", keyForRight.ToString());
+        PlayerPrefs.SetString("JumpKey", playerMovement.jumpKey.ToString());
+        PlayerPrefs.SetString("DashKey", playerMovement.dashKey.ToString());
+        PlayerPrefs.SetString("InteractKey", playerMovement.interactKey.ToString());
+
+        PlayerPrefs.Save(); // Speichern in die Datei
+        Debug.Log("Änderungen gespeichert.");
+    }
+
 
     public void QuitGame()
     {
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,235 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+
+    [Header("Movement")]
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    private float dashSpeed = 15;
+
+    private Rigidbody2D body;
+    private Animator animator;
+    private BoxCollider2D boxCollider;
+
+    // walljump has a cooldown so you cant spam walljump
+    private float wallJumpCooldown;
+    private GameObject cam;
+
+    private bool isMovementEnabled = true;
+
+    [Header("Knockback")]
+    public float KBForce;
+    public float KBCounter;
+    public float KBTotalTime;
+
+    public bool KnockFromRight;
+    public int coin;
+
+    private bool isDashing;
+
+    [SerializeField] private DialogueUI dialogueUI;
+
+    public DialogueUI DialogueUI => dialogueUI;
+
+    public IInteractable Interactable { get; set; }
+
+    public KeyCode moveLeftKey = KeyCode.A;
+    public KeyCode moveRightKey = KeyCode.D;
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode dashKey = KeyCode.LeftShift;
+    public KeyCode interactKey = KeyCode.E;
+
+    void Start()
+    {
+        LoadControls();
+    }
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        cam = GameObject.FindGameObjectWithTag("PlayerCam");
+    }
+
+    private void Update()
+    {
+        if (dialogueUI.IsOpen)
+            return;
+
+        float horizontalInput = 0f;
+        
+        if (isMovementEnabled)
+        {
+            if (Input.GetKey(moveLeftKey))
+                horizontalInput = -1f;
+            else if (Input.GetKey(moveRightKey))
+                horizontalInput = 1f;
+        }
+
+        if (KBCounter <= 0)
+        {
+            flipCharacter(horizontalInput);
+
+            // Animator-Einstellungen
+            animator.SetBool("isRunning", horizontalInput != 0);
+            animator.SetBool("isGrounded", isGrounded());
+
+            // Dash-Logik
+            if (Input.GetKeyDown(dashKey) && isGrounded())
+                dash(horizontalInput);
+            else if (!isDashing)
+                body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+            // Wall-Jump-Logik
+            if (wallJumpCooldown > 0.2f && KBCounter <= 0)
+            {
+                handleWallJump(horizontalInput);
+            }
+            else
+                wallJumpCooldown += Time.deltaTime;
+
+            // Sprung-Logik
+            if (Input.GetKey(jumpKey))
+                Jump();
+
+            // Interaktions-Logik
+            if (Input.GetKeyDown(interactKey))
+                Interactable?.Interact(this);
+        }
+        else
+        {
+            handleKnockback(horizontalInput);
+        }
+    }
+
+    private void Jump()
+    {
+        if (isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            animator.SetTrigger("jump");
+        }
+        else if (isOnWall() && !isGrounded())
+        {
+            wallJumpCooldown = 0;
+            body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x), 18);
+        }
+    }
+
+    private void handleKnockback(float horizontalInput)
+    {
+        if (KBCounter > 0)
+        {
+            if (KnockFromRight)
+                body.velocity = new Vector2(-KBForce, KBForce / 2);
+            else
+                body.velocity = new Vector2(KBForce, KBForce / 2);
+
+            KBCounter -= Time.deltaTime;
+        }
+    }
+
+    private void handleWallJump(float horizontalInput)
+    {
+        if (isOnWall())
+        {
+            body.gravityScale = 8;
+            body.velocity = Vector2.zero;
+        }
+        else
+            body.gravityScale = 4f;
+    }
+
+    private void flipCharacter(float horizontalInput)
+    {
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(0.135f, 0.135f, 1);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-0.135f, 0.135f, 1);
+    }
+
+    private void dash(float horizontalInput)
+    {
+        if (!isDashing && isGrounded())
+        {
+            isDashing = true;
+            body.velocity = new Vector2(horizontalInput * dashSpeed, body.velocity.y);
+            animator.SetTrigger("dodge");
+        }
+        StartCoroutine(endDash());
+    }
+
+    private IEnumerator endDash()
+    {
+        yield return new WaitForSeconds(0.5f);
+        isDashing = false;
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool isOnWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+
+    public bool canAttack()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        return horizontalInput == 0 && isGrounded() && !isOnWall();
+    }
+
+    public void disableMovement()
+    {
+        isMovementEnabled = false;
+    }
+
+    public void enableMovement()
+    {
+        isMovementEnabled = true;
+    }
+
+    public void SetControls(KeyCode newMoveLeft, KeyCode newMoveRight, KeyCode newJump, KeyCode newDash, KeyCode newInteract)
+    {
+        moveLeftKey = newMoveLeft;
+        moveRightKey = newMoveRight;
+        jumpKey = newJump;
+        dashKey = newDash;
+        interactKey = newInteract;
+    }
+
+    public void LoadControls()
+    {
+        // Laden der Tastenbelegungen aus PlayerPrefs
+        if (PlayerPrefs.HasKey("MoveLeftKey"))
+            moveLeftKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("MoveLeftKey"));
+
+        if (PlayerPrefs.HasKey("MoveRightKey"))
+            moveRightKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("MoveRightKey"));
+
+        if (PlayerPrefs.HasKey("JumpKey"))
+            jumpKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("JumpKey"));
+
+        if (PlayerPrefs.HasKey("DashKey"))
+            dashKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("DashKey"));
+
+        if (PlayerPrefs.HasKey("InteractKey"))
+            interactKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("InteractKey"));
+
+        Debug.Log("Kontrollen geladen.");
+    }
+}
+
+/*
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -231,3 +463,4 @@ public class PlayerMovement : MonoBehaviour
         isMovementEnabled = true;
     }
 }
+*/
